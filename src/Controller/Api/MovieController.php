3ac168beeb329aa9268d8b77c0a110f0826c89ee
Galluchot <mickael.galluchot@gmail.com>
2021-03-23 17:27:33 +0100

    Read one Movie

--- src/Controller/Api/MovieController.php
+++ src/Controller/Api/MovieController.php
@@ -27,5 +27,37 @@ class MovieController extends AbstractController
         return $this->json($movies, 200, [], ['groups' => 'movies_read']);
     }
 
+        /**
+     * Read one movie
+     * 
+     * @Route("/api/movies/{id<\d+>}", name="api_movies_read_item", methods="GET")
+     */
+    public function readItem(Movie $movie = null): Response
+    {
+        // 404 ?
+        if ($movie === null) {
+
+            // Optionnel, message pour le front
+            $message = [
+                'status' => Response::HTTP_NOT_FOUND,
+                'error' => 'Film non trouvé.',
+            ];
+
+            // On défini un message custom et un status code HTTP 404
+            return $this->json($message, Response::HTTP_NOT_FOUND);
+        }
+
+        // @todo Tenter d'utiliser la requête custom
+        // de jointure sur castings et persons
+
+        // Le 4ème argument représente le "contexte"
+        // qui sera transmis au Serializer
+        return $this->json($movie, 200, [], ['groups' => [
+                'movies_read',
+            ]
+        ]);
+    }
+
+
 }
 
